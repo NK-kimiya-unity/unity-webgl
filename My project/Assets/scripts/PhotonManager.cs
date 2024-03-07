7c822654e74c563b50473200922816a123942d16
kimi <programassets6@gmail.com>
2024-03-07 17:52:10 +0900

    オンライン接続処理のルームの作成の実装

--- My project/Assets/scripts/PhotonManager.cs
+++ My project/Assets/scripts/PhotonManager.cs
@@ -2,18 +2,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
+//photonViewと、PUNが呼び出すことのできるすべてのコールバック/イベントを提供します。使用したいイベント/メソッドをオーバーライドしてください。
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
-   public static PhotonManager instance;//static
-   public GameObject loadingPanel;//ロードパネル
-   public Text loadingText;//ロードテキスト
-   public GameObject buttons;//ボタン
+    //よく見るドキュメントページ
+    //https://doc-api.photonengine.com/ja-jp/pun/current/class_photon_1_1_pun_1_1_photon_network.html
+    //https://doc-api.photonengine.com/ja-jp/pun/current/class_photon_1_1_pun_1_1_mono_behaviour_pun_callbacks.html
+    //https://doc-api.photonengine.com/ja-jp/pun/current/namespace_photon_1_1_realtime.html
+
+    public static PhotonManager instance;//static
+    public GameObject loadingPanel;//ロードパネル
+    public Text loadingText;//ロードテキスト
+    public GameObject buttons;//ボタン
+
+
+    public GameObject createRoomPanel;//ルーム作成パネル
+    public Text enterRoomName;//入力されたルーム名テキスト
+
 
     private void Awake()
     {
         instance = this;//格納
     }
+
 
     void Start()
     {
@@ -33,13 +46,25 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     }
 
 
+    /// <summary>
+    /// 一旦すべてを非表示にする
+    /// </summary>
     void CloseMenuUI()//なぜ作るのか：UI切り替えが非常に楽だから
     {
         loadingPanel.SetActive(false);//ロードパネル非表示
 
         buttons.SetActive(false);//ボタン非表示
+
+        createRoomPanel.SetActive(false);//ルーム作成パネル
     }
 
+
+
+    //継承元のメソッドでは「virtual」のキーワード
+    //継承先では「override」のキーワード
+    /// <summary>
+    /// クライアントがMaster Serverに接続されていて、マッチメイキングやその他のタスクを行う準備が整ったときに呼び出されます
+    /// </summary>
     public override void OnConnectedToMaster()//
     {
 
@@ -49,15 +74,49 @@ public class PhotonManager : MonoBehaviourPunCallbacks
 
     }
 
-      public void LobbyMenuDisplay()
+
+    /// <summary>
+    /// マスターサーバーのロビーに入るときに呼び出されます。
+    /// </summary>
+    public override void OnJoinedLobby()//
+    {
+
+        LobbyMenuDisplay();//
+
+    }
+
+
+    //ロビーメニュー表示(エラーパネル閉じる時もこれ)
+    public void LobbyMenuDisplay()
     {
         CloseMenuUI();
         buttons.SetActive(true);
     }
 
-    public override void OnJoinedLobby()//
+    //タイトルの部屋作成ボタン押下時に呼ぶ：UIから呼び出す
+    public void OpenCreateRoomPanel()
     {
-        LobbyMenuDisplay();//
+        CloseMenuUI();
+        createRoomPanel.SetActive(true);
     }
 
+    //部屋作成ボタン押下時に呼ぶ：UIから呼び出す
+    public void CreateRoomButton()
+    {
+        //インプットフィールドのテキストに何か入力されていた場合
+        if (!string.IsNullOrEmpty(enterRoomName.text))
+        {
+            //ルームのオプションをインスタンス化して変数に入れる 
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = 6;// プレイヤーの最大参加人数の設定（無料版は20まで。1秒間にやり取りできるメッセージ数に限りがあるので10以上は難易度上がる）
+
+            //ルームを作る(ルーム名：部屋の設定)
+            PhotonNetwork.CreateRoom(enterRoomName.text, options);
+
+
+            CloseMenuUI();//メニュー閉じる
+            loadingText.text = "ルーム作成中...";
+            loadingPanel.SetActive(true);
+        }
+    }
 }
